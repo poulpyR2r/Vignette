2a6f0b8e3359c69673cd9d3934b2374e3ecfd8a9
Guewen Carre <guewencarre@gmail.com>
2023-02-01 14:42:35 +0100

    upload sur home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,19 +1,20 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Photo;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Route; 
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(ManagerRegistry $doctrine): Response
     {
-        $photo = $doctrine->getRepository(PhotoFormType::class)->findAll();
+        $photos = $doctrine->getRepository(Photo::class)->findAll();
         return $this->render('home/index.html.twig', [
-            'vignettes' => $vignettes,
+            'photos' => $photos,
            
         ]);
     }
